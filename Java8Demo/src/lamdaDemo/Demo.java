05e31ced2eafcef2e5a62907726ab01c9ee22b93
sagargohad <sagarbgohad7@gmai.com>
2018-10-26 22:38:46 +0530

    My lamda example

---
+++ Java8Demo/src/lamdaDemo/Demo.java
@@ -0,0 +1,13 @@
+package lamdaDemo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		FInterface fi = (int x) -> System.out.println(2 * x);
+		
+		fi.add(5);
+		
+
+	}
+
+}
---
+++ Java8Demo/src/lamdaDemo/FInterface.java
@@ -0,0 +1,10 @@
+package lamdaDemo;
+
+public interface FInterface {
+	void add(int x);
+	
+	default public void getName(){
+		System.out.println("Sagar");
+	}
+
+}
